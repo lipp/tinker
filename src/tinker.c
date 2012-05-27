807a695c58c67d63e3ce183d3fd30cb6ef868267
lipp <gelipp@googlemail.com>
2012-05-27 17:40:26 +0200

    button pressed support

--- src/tinker.c
+++ src/tinker.c
@@ -16,7 +16,19 @@
 struct tinker_ipcon {
   lua_State *L;
   IPConnection ipcon;
+  int enumerate_callback_ref;
 };
+
+struct tinker_lcd20x4 {
+  LCD20x4 lcd;
+  int index;
+  lua_State *L;
+  int button_pressed_callback_ref;
+  int button_released_callback_ref;
+};
+
+static struct tinker_lcd20x4 * lcd20x4_array[] = {NULL, NULL, NULL, NULL};
+
 
 static int tinker_ipcon(lua_State *L) {
   int port = luaL_optint(L, 2, 4223);
@@ -34,57 +46,109 @@ static int tinker_ipcon(lua_State *L) {
 }
 
 static int tinker_ipcon_destroy(lua_State* L) {
-  IPConnection *ipcon = (IPConnection *)luaL_checkudata(L, 1, TINKER_IPCON_META);
+  IPConnection *ipcon = luaL_checkudata(L, 1, TINKER_IPCON_META);
   ipcon_destroy(ipcon);
   return 0;
 }
 
+static int tinker_ipcon_join_thread(lua_State* L) {
+  IPConnection *ipcon = luaL_checkudata(L, 1, TINKER_IPCON_META);
+  ipcon_join_thread(ipcon);
+  return 0;
+}
+
 static int tinker_ipcon_lcd20x4(lua_State* L) {
-  IPConnection *ipcon = (IPConnection *)luaL_checkudata(L, 1, TINKER_IPCON_META);
-  const char* uid = luaL_checkstring(L, 2); 
-  LCD20x4 *lcd = lua_newuserdata(L, sizeof(LCD20x4));
-  lcd_20x4_create(lcd, uid);
+  IPConnection *ipcon = luaL_checkudata(L, 1, TINKER_IPCON_META);
+  const char* uid = luaL_checkstring(L, 2);
+  int index = 0;
+  while(lcd20x4_array[index] && index < sizeof(lcd20x4_array)) {
+    ++index;
+  }
+  if(lcd20x4_array[index]) {
+    luaL_error(L, "no free lcd20x4 slot");
+  }
+  struct tinker_lcd20x4 *tlcd = lua_newuserdata(L, sizeof(struct tinker_lcd20x4));
+  lcd20x4_array[index] = tlcd;
+  tlcd->index = index;
+  tlcd->L = L;
+  lcd_20x4_create(&tlcd->lcd, uid);
+  tlcd->button_pressed_callback_ref = LUA_REFNIL;
+  tlcd->button_released_callback_ref = LUA_REFNIL;
   luaL_getmetatable(L, TINKER_LCD20x4_META);
   lua_setmetatable(L, -2);
   
-  if(ipcon_add_device(ipcon, lcd) < 0) {
+  if(ipcon_add_device(ipcon, &tlcd->lcd) < 0) {
     luaL_error(L, "ipcon_add_device failed");
   }
   return 1;
 }
 
+#define DECL_TINKER_LCD20X4_ON_BUTTON_PRESSED(X)                        \
+  static void tinker_lcd20x4_on_button_pressed_##X(uint8_t button) {    \
+  lua_State *L = lcd20x4_array[X]->L;                                        \
+  lua_rawgeti(L, LUA_REGISTRYINDEX, lcd20x4_array[X]->button_pressed_callback_ref); \
+  if(!lua_isfunction(L, -1)) {                                          \
+    lua_pop(L, 1);                                                      \
+    return;                                                             \
+  }                                                                     \
+  lua_pushnumber(L, button);                                            \
+lua_call(L, 1, 0);                              \
+}                                               \
+
+DECL_TINKER_LCD20X4_ON_BUTTON_PRESSED(0);
+DECL_TINKER_LCD20X4_ON_BUTTON_PRESSED(1);
+DECL_TINKER_LCD20X4_ON_BUTTON_PRESSED(2);
+DECL_TINKER_LCD20X4_ON_BUTTON_PRESSED(3);
+
+void *tinker_lcd20x4_on_button_pressed_callbacks[] = {
+  tinker_lcd20x4_on_button_pressed_0,
+  tinker_lcd20x4_on_button_pressed_1,
+  tinker_lcd20x4_on_button_pressed_2,
+  tinker_lcd20x4_on_button_pressed_3
+};
+
+static int tinker_lcd20x4_button_pressed(lua_State *L) {
+  struct tinker_lcd20x4 *tlcd = luaL_checkudata(L, 1, TINKER_LCD20x4_META);
+  luaL_checktype(L, 2, LUA_TFUNCTION);
+  tlcd->button_pressed_callback_ref = luaL_ref(L, LUA_REGISTRYINDEX);
+  lcd_20x4_register_callback(&tlcd->lcd, 
+                             LCD_20X4_CALLBACK_BUTTON_PRESSED, 
+                             tinker_lcd20x4_on_button_pressed_callbacks[tlcd->index]);
+  return 0;
+}
+
 static int tinker_lcd20x4_backlight_on(lua_State* L) {
-  LCD20x4 *lcd = (LCD20x4 *)luaL_checkudata(L, 1, TINKER_LCD20x4_META);
-  lcd_20x4_backlight_on(lcd);
+  struct tinker_lcd20x4 *tlcd = luaL_checkudata(L, 1, TINKER_LCD20x4_META);
+  lcd_20x4_backlight_on(&tlcd->lcd);
   return 0;
 }
 
 static int tinker_lcd20x4_backlight_off(lua_State* L) {
-  LCD20x4 *lcd = (LCD20x4 *)luaL_checkudata(L, 1, TINKER_LCD20x4_META);
-  lcd_20x4_backlight_off(lcd);
+  struct tinker_lcd20x4 *tlcd = luaL_checkudata(L, 1, TINKER_LCD20x4_META);
+  lcd_20x4_backlight_off(&tlcd->lcd);
   return 0;
 }
 
 static int tinker_lcd20x4_clear_display(lua_State* L) {
-  LCD20x4 *lcd = (LCD20x4 *)luaL_checkudata(L, 1, TINKER_LCD20x4_META);
-  lcd_20x4_clear_display(lcd);
+  struct tinker_lcd20x4 *tlcd = luaL_checkudata(L, 1, TINKER_LCD20x4_META);
+  lcd_20x4_clear_display(&tlcd->lcd);
   return 0;
 }
 
 static int tinker_lcd20x4_is_backlight_on(lua_State* L) {
-  LCD20x4 *lcd = (LCD20x4 *)luaL_checkudata(L, 1, TINKER_LCD20x4_META);
+  struct tinker_lcd20x4 *tlcd = luaL_checkudata(L, 1, TINKER_LCD20x4_META);
   bool on;
-  lcd_20x4_is_backlight_on(lcd, &on);
+  lcd_20x4_is_backlight_on(&tlcd->lcd, &on);
   lua_pushboolean(L, on);
   return 1;
 }
 
 static int tinker_lcd20x4_write_line(lua_State* L) {
-  LCD20x4 *lcd = (LCD20x4 *)luaL_checkudata(L, 1, TINKER_LCD20x4_META);
+  struct tinker_lcd20x4 *tlcd = luaL_checkudata(L, 1, TINKER_LCD20x4_META);
   int line = luaL_checkinteger(L, 2);
   int position = luaL_checkinteger(L, 3);
-  char *text = luaL_checkstring(L, 4);
-  lcd_20x4_write_line(lcd, line, position, text);
+  const char *text = luaL_checkstring(L, 4);
+  lcd_20x4_write_line(&tlcd->lcd, line, position, (char *)text);
   return 0;
 }
 
@@ -97,10 +161,12 @@ static const struct luaL_Reg tinker_ipcon_methods [] = {
   {"destroy",tinker_ipcon_destroy},
   {"__gc",tinker_ipcon_destroy},
   {"lcd20x4",tinker_ipcon_lcd20x4},
+  {"join_thread",tinker_ipcon_join_thread},
   {NULL,NULL}
 };
 
 static const struct luaL_Reg tinker_lcd20x4_methods [] = {
+  {"button_pressed",tinker_lcd20x4_button_pressed},
   {"backlight_on",tinker_lcd20x4_backlight_on},
   {"backlight_off",tinker_lcd20x4_backlight_off},
   {"is_backlight_on",tinker_lcd20x4_is_backlight_on},
